a45390a8656ae63b129541dc5077200700278065
Kelvin S. Prado <kelvinpfw@hotmail.com>
2017-05-29 11:02:50 -0300

    Creating model functions

--- model.go
+++ model.go
@@ -3,32 +3,57 @@
 package main
 
 import (
-    "database/sql"
-    "errors"
+	"database/sql"
 )
 
 type user struct {
-    ID    int    `json:"id"`
-    Name  string `json:"name"`
-    Age   int    `json:"age"`
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	Age  int    `json:"age"`
 }
 
 func (u *user) getUser(db *sql.DB) error {
-    return errors.New("Not implemented")
+	return db.QueryRow("SELECT name, age FROM users WHERE id=$1", u.ID).Scan(&u.Name, &u.Age)
 }
 
 func (u *user) updateUser(db *sql.DB) error {
-    return errors.New("Not implemented")
+	_, err := db.Exec("UPDATE users SET name=$1, age=$2 WHERE id=$3", u.Name, u.Age, u.ID)
+	return err
 }
 
 func (u *user) deleteUser(db *sql.DB) error {
-    return errors.New("Not implemented")
+	_, err := db.Exec("DELETE FROM users WHERE id=$1", u.ID)
+	return err
 }
 
 func (u *user) createUser(db *sql.DB) error {
-    return errors.New("Not implemented")
+	err := db.QueryRow("INSERT INTO users(name, age) VALUES($1, $2) RETURNING id", u.Name, u.Age).Scan(&u.ID)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func getUsers(db *sql.DB, start, count int) ([]user, error) {
-    return nil, errors.New("Not implemented")
+	rows, err := db.Query("SELECT id, name, age FROM users LIMIT $1 OFFSET $2", count, start)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	users := []user{}
+
+	for rows.Next() {
+		var u user
+		if err := rows.Scan(&u.ID, &u.Name, &u.Age); err != nil {
+			return nil, err
+		}
+		users = append(users, u)
+	}
+
+	return users, nil
 }
