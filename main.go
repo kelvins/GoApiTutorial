97bcb96639f12cb60a162e21e2ff79ffd9cee81e
Kelvin S. Prado <kelvinpfw@hotmail.com>
2017-05-29 11:03:27 -0300

    Applying gofmt :art:

--- main.go
+++ main.go
@@ -3,8 +3,8 @@
 package main
 
 func main() {
-    a := App{}
-    a.Initialize("DB_USERNAME", "DB_PASSWORD", "rest_api_example")
+	a := App{}
+	a.Initialize("DB_USERNAME", "DB_PASSWORD", "rest_api_example")
 
-    a.Run(":8080")
+	a.Run(":8080")
 }
--- main_test.go
+++ main_test.go
@@ -3,42 +3,42 @@
 package main_test
 
 import (
-    "os"
-    "log"
-    "bytes"
-    "strconv"
-    "net/http"
-    "encoding/json"
-    "net/http/httptest"
-    "testing"
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"strconv"
+	"testing"
 
-    "github.com/kelvins/goapi"
+	"github.com/kelvins/goapi"
 )
 
 var a main.App
 
 func TestMain(m *testing.M) {
-    a = main.App{}
-    a.Initialize("root", "", "rest_api_example")
+	a = main.App{}
+	a.Initialize("root", "", "rest_api_example")
 
-    ensureTableExists()
+	ensureTableExists()
 
-    code := m.Run()
+	code := m.Run()
 
-    clearTable()
+	clearTable()
 
-    os.Exit(code)
+	os.Exit(code)
 }
 
 func ensureTableExists() {
-    if _, err := a.DB.Exec(tableCreationQuery); err != nil {
-        log.Fatal(err)
-    }
+	if _, err := a.DB.Exec(tableCreationQuery); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func clearTable() {
-    a.DB.Exec("DELETE FROM users")
-    a.DB.Exec("ALTER TABLE users AUTO_INCREMENT = 1")
+	a.DB.Exec("DELETE FROM users")
+	a.DB.Exec("ALTER TABLE users AUTO_INCREMENT = 1")
 }
 
 const tableCreationQuery = `
@@ -50,140 +50,140 @@ CREATE TABLE IF NOT EXISTS users
 )`
 
 func TestEmptyTable(t *testing.T) {
-    clearTable()
+	clearTable()
 
-    req, _ := http.NewRequest("GET", "/users", nil)
-    response := executeRequest(req)
+	req, _ := http.NewRequest("GET", "/users", nil)
+	response := executeRequest(req)
 
-    checkResponseCode(t, http.StatusOK, response.Code)
+	checkResponseCode(t, http.StatusOK, response.Code)
 
-    if body := response.Body.String(); body != "[]" {
-        t.Errorf("Expected an empty array. Got %s", body)
-    }
+	if body := response.Body.String(); body != "[]" {
+		t.Errorf("Expected an empty array. Got %s", body)
+	}
 }
 
 func executeRequest(req *http.Request) *httptest.ResponseRecorder {
-    rr := httptest.NewRecorder()
-    a.Router.ServeHTTP(rr, req)
+	rr := httptest.NewRecorder()
+	a.Router.ServeHTTP(rr, req)
 
-    return rr
+	return rr
 }
 
 func checkResponseCode(t *testing.T, expected, actual int) {
-    if expected != actual {
-        t.Errorf("Expected response code %d. Got %d\n", expected, actual)
-    }
+	if expected != actual {
+		t.Errorf("Expected response code %d. Got %d\n", expected, actual)
+	}
 }
 
 func TestGetNonExistentUser(t *testing.T) {
-    clearTable()
+	clearTable()
 
-    req, _ := http.NewRequest("GET", "/user/45", nil)
-    response := executeRequest(req)
+	req, _ := http.NewRequest("GET", "/user/45", nil)
+	response := executeRequest(req)
 
-    checkResponseCode(t, http.StatusNotFound, response.Code)
+	checkResponseCode(t, http.StatusNotFound, response.Code)
 
-    var m map[string]string
-    json.Unmarshal(response.Body.Bytes(), &m)
-    if m["error"] != "User not found" {
-        t.Errorf("Expected the 'error' key of the response to be set to 'User not found'. Got '%s'", m["error"])
-    }
+	var m map[string]string
+	json.Unmarshal(response.Body.Bytes(), &m)
+	if m["error"] != "User not found" {
+		t.Errorf("Expected the 'error' key of the response to be set to 'User not found'. Got '%s'", m["error"])
+	}
 }
 
 func TestCreateUser(t *testing.T) {
-    clearTable()
+	clearTable()
 
-    payload := []byte(`{"name":"test user","30`)
+	payload := []byte(`{"name":"test user","30`)
 
-    req, _ := http.NewRequest("POST", "/user", bytes.NewBuffer(payload))
-    response := executeRequest(req)
+	req, _ := http.NewRequest("POST", "/user", bytes.NewBuffer(payload))
+	response := executeRequest(req)
 
-    checkResponseCode(t, http.StatusCreated, response.Code)
+	checkResponseCode(t, http.StatusCreated, response.Code)
 
-    var m map[string]interface{}
-    json.Unmarshal(response.Body.Bytes(), &m)
+	var m map[string]interface{}
+	json.Unmarshal(response.Body.Bytes(), &m)
 
-    if m["name"] != "test user" {
-        t.Errorf("Expected user name to be 'test user'. Got '%v'", m["name"])
-    }
+	if m["name"] != "test user" {
+		t.Errorf("Expected user name to be 'test user'. Got '%v'", m["name"])
+	}
 
-    if m["age"] != 30 {
-        t.Errorf("Expected user age to be '30'. Got '%v'", m["price"])
-    }
+	if m["age"] != 30 {
+		t.Errorf("Expected user age to be '30'. Got '%v'", m["price"])
+	}
 
-    // the id is compared to 1.0 because JSON unmarshaling converts numbers to
-    // floats, when the target is a map[string]interface{}
-    if m["id"] != 1.0 {
-        t.Errorf("Expected product ID to be '1'. Got '%v'", m["id"])
-    }
+	// the id is compared to 1.0 because JSON unmarshaling converts numbers to
+	// floats, when the target is a map[string]interface{}
+	if m["id"] != 1.0 {
+		t.Errorf("Expected product ID to be '1'. Got '%v'", m["id"])
+	}
 }
 
 func addUsers(count int) {
-    if count < 1 {
-        count = 1
-    }
+	if count < 1 {
+		count = 1
+	}
 
-    for i := 0; i < count; i++ {
-        a.DB.Exec("INSERT INTO users(name, age) VALUES($1, $2)", "User "+strconv.Itoa(i), (i*10))
-    }
+	for i := 0; i < count; i++ {
+		a.DB.Exec("INSERT INTO users(name, age) VALUES($1, $2)", "User "+strconv.Itoa(i), (i * 10))
+	}
 }
 
 func TestGetUser(t *testing.T) {
-    clearTable()
-    addUsers(1)
+	clearTable()
+	addUsers(1)
 
-    req, _ := http.NewRequest("GET", "/user/1", nil)
-    response := executeRequest(req)
+	req, _ := http.NewRequest("GET", "/user/1", nil)
+	response := executeRequest(req)
 
-    checkResponseCode(t, http.StatusOK, response.Code)
+	checkResponseCode(t, http.StatusOK, response.Code)
 }
 
 func TestUpdateUser(t *testing.T) {
-    clearTable()
-    addUsers(1)
+	clearTable()
+	addUsers(1)
 
-    req, _ := http.NewRequest("GET", "/user/1", nil)
-    response := executeRequest(req)
-    var originalUser map[string]interface{}
-    json.Unmarshal(response.Body.Bytes(), &originalUser)
+	req, _ := http.NewRequest("GET", "/user/1", nil)
+	response := executeRequest(req)
+	var originalUser map[string]interface{}
+	json.Unmarshal(response.Body.Bytes(), &originalUser)
 
-    payload := []byte(`{"name":"test user - updated name","age":21}`)
+	payload := []byte(`{"name":"test user - updated name","age":21}`)
 
-    req, _ = http.NewRequest("PUT", "/user/1", bytes.NewBuffer(payload))
-    response = executeRequest(req)
+	req, _ = http.NewRequest("PUT", "/user/1", bytes.NewBuffer(payload))
+	response = executeRequest(req)
 
-    checkResponseCode(t, http.StatusOK, response.Code)
+	checkResponseCode(t, http.StatusOK, response.Code)
 
-    var m map[string]interface{}
-    json.Unmarshal(response.Body.Bytes(), &m)
+	var m map[string]interface{}
+	json.Unmarshal(response.Body.Bytes(), &m)
 
-    if m["id"] != originalUser["id"] {
-        t.Errorf("Expected the id to remain the same (%v). Got %v", originalUser["id"], m["id"])
-    }
+	if m["id"] != originalUser["id"] {
+		t.Errorf("Expected the id to remain the same (%v). Got %v", originalUser["id"], m["id"])
+	}
 
-    if m["name"] == originalUser["name"] {
-        t.Errorf("Expected the name to change from '%v' to '%v'. Got '%v'", originalUser["name"], m["name"], m["name"])
-    }
+	if m["name"] == originalUser["name"] {
+		t.Errorf("Expected the name to change from '%v' to '%v'. Got '%v'", originalUser["name"], m["name"], m["name"])
+	}
 
-    if m["age"] == originalUser["age"] {
-        t.Errorf("Expected the age to change from '%v' to '%v'. Got '%v'", originalUser["age"], m["age"], m["age"])
-    }
+	if m["age"] == originalUser["age"] {
+		t.Errorf("Expected the age to change from '%v' to '%v'. Got '%v'", originalUser["age"], m["age"], m["age"])
+	}
 }
 
 func TestDeleteUser(t *testing.T) {
-    clearTable()
-    addUsers(1)
+	clearTable()
+	addUsers(1)
 
-    req, _ := http.NewRequest("GET", "/user/1", nil)
-    response := executeRequest(req)
-    checkResponseCode(t, http.StatusOK, response.Code)
+	req, _ := http.NewRequest("GET", "/user/1", nil)
+	response := executeRequest(req)
+	checkResponseCode(t, http.StatusOK, response.Code)
 
-    req, _ = http.NewRequest("DELETE", "/user/1", nil)
-    response = executeRequest(req)
+	req, _ = http.NewRequest("DELETE", "/user/1", nil)
+	response = executeRequest(req)
 
-    checkResponseCode(t, http.StatusOK, response.Code)
+	checkResponseCode(t, http.StatusOK, response.Code)
 
-    req, _ = http.NewRequest("GET", "/user/1", nil)
-    response = executeRequest(req)
-    checkResponseCode(t, http.StatusNotFound, response.Code)
+	req, _ = http.NewRequest("GET", "/user/1", nil)
+	response = executeRequest(req)
+	checkResponseCode(t, http.StatusNotFound, response.Code)
 }
